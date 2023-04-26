219428ae4caa29e5ff71fd1faa4cef073c350c41
yr <y315483585@163.com>
2023-04-27 00:18:40 +0800

    增加logo，退出问候和一个翻译工具

---
+++ translate/zh-cn.go
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023. YR. All rights reserved
+ */
+
+// Package translate
+// 模块名: 模块名
+// 功能描述: 描述
+// 作者:  yr  2023/4/26 0026 23:01
+// 最后更新:  yr  2023/4/26 0026 23:01
+package translate
+
+func init() {
+	register(ZH_CN, zhCnMap)
+}
+
+var zhCnMap = map[string]string{
+	"Press enter key to exit...": "按回车键退出...",  // 回车键退出
+	"Version":                    "版本号",        // 版本
+	"Supported by":               "由chaos提供支持", // 提供支持
+}
