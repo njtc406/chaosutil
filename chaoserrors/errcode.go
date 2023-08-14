aaa6ceeae8817a08f1a2ec83bcaca0bbde150cfa
yr <y315483585@163.com>
2023-08-14 14:55:16 +0800

    其他优化

---
+++ chaoserrors/errcode.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023. YR. All rights reserved
+ */
+
+// Package chaoserrors
+// 模块名: 模块名
+// 功能描述: 描述
+// 作者:  yr  2023/7/23 0023 13:12
+// 最后更新:  yr  2023/7/23 0023 13:12
+package chaoserrors
+
+const (
+	Success   = 0 // 成功
+	IsRunning = 1 // 运行中
+)
